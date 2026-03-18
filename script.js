3f8916328e4526eee7810010af35d2cbc29fb078
deadgothangel <rogov103342@gmail.com>
2026-03-18 18:00:41 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const timerElement = document.getElementById('timer');
+const audio = document.getElementById('bg-audio');
 
 function glitchTimer() {
-    // Безумная генерация цифр
     let mm = Math.floor(Math.random() * 99).toString().padStart(2, '0');
     let ss = Math.floor(Math.random() * 99).toString().padStart(2, '0');
     let ms = Math.floor(Math.random() * 99).toString().padStart(2, '0');
@@ -9,13 +9,12 @@ function glitchTimer() {
     timerElement.innerText = `00:${mm}:${ss}:${ms}`;
 }
 
-// Обновление каждые 50мс для эффекта поломки
 setInterval(glitchTimer, 50);
 
 function openModal() {
     document.getElementById('modal').style.display = "block";
-    // Включаем музыку, если она заблокирована браузером
-    document.getElementById('bg-audio').play();
+    // Попытка запустить музыку при клике (обход блоков браузера)
+    audio.play().catch(e => console.log("Audio play blocked"));
 }
 
 function closeModal() {
